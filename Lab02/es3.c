139df7de1fc81f2d5e510577708d19418619862d
fab <fabmi1234@gmail.com>
2022-03-17 19:27:35 +0100

    rename variables exercise 3

--- Lab02/es3.c
+++ Lab02/es3.c
@@ -7,15 +7,16 @@
 #include <fcntl.h>
 #include <string.h>
 
-#define N 600
+#define N 256
+#define L 1024
 #define BUFSIZE 2048
 
 void cd_function(char *dir1, char *dir2){
-    struct stat buf1, buf2;
-    DIR *dp1, *dp2;
-    char fullName[N], fullName2[N], fullName3[N], fullName4[40], buf[BUFSIZE];
-    struct dirent *dirp1, *dirp2;
-    int i, nR, nW, fdR, fdW;
+    struct stat buf1;
+    DIR *dp1;
+    char sourceFile[L], destFile[L], dir2P[L], dir1P[L], buf[BUFSIZE];
+    struct dirent *dirp1;
+    int nR, nW, fdR, fdW;
 
     dp1 = opendir(dir1);
     while((dirp1 = readdir(dp1)) != NULL){
@@ -24,20 +25,20 @@ void cd_function(char *dir1, char *dir2){
             continue;
        
 
-        sprintf(fullName, "%s/%s", dir1, dirp1->d_name);
-        printf("file sorgente %s\n", fullName);
+        sprintf(sourceFile, "%s/%s", dir1, dirp1->d_name);
+        printf("Source File %s\n", sourceFile);
 
-        stat(fullName, &buf1);
+        stat(sourceFile, &buf1);
        
         if(S_ISDIR(buf1.st_mode) == 0){
             
-            fdR = open(fullName, O_RDONLY); 
+            fdR = open(sourceFile, O_RDONLY); 
            
-            sprintf(fullName2, "%s/%s", dir2, dirp1->d_name);
+            sprintf(destFile, "%s/%s", dir2, dirp1->d_name);
            
-            printf("file destinazione %s\n", fullName2);
+            printf("file destinazione %s\n", destFile);
 
-            fdW = open(fullName2, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+            fdW = open(destFile, O_WRONLY | O_CREAT | O_TRUNC, 0666);
            
             while((nR = read(fdR, buf, BUFSIZE)) > 0){
                 printf("%s\n", buf);
@@ -46,12 +47,10 @@ void cd_function(char *dir1, char *dir2){
                 printf("%d\n", nW);
             }
         } else {
-            sprintf(fullName3, "%s/%s", dir2, dirp1->d_name);
-            sprintf(fullName4, "%s/%s", dir1, dirp1->d_name);
-            //printf("%s\n", fullName3);
-            //printf("%s\n", fullName4);
-            mkdir(fullName3, S_IRWXU);
-            cd_function(fullName4, fullName3);
+            sprintf(dir2P, "%s/%s", dir2, dirp1->d_name);
+            sprintf(dir1P, "%s/%s", dir1, dirp1->d_name);
+            mkdir(dir2P, S_IRWXU);
+            cd_function(dir1P, dir2P);
         }
         
     }
@@ -59,8 +58,7 @@ void cd_function(char *dir1, char *dir2){
 }
 
 int main(int argc, char **argv){
-    DIR *dp;
-    struct dirent *dirp;
+    
     if(argc != 3)
         printf("Error\n");
     cd_function(argv[1], argv[2]);
