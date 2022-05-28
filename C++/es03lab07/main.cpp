f94444dd7566a07a3b0df3829b9b4fe3a558161c
Fabio Orazio Mirto <fabmi1234@gmail.com>
2022-05-28 18:14:04 +0200

    add exercise 3 of lab7 from template of cpp-slide-9

--- C++/es03lab07/main.cpp
+++ C++/es03lab07/main.cpp
@@ -1,71 +1,80 @@
-#include <iostream>
+#include "condition_variable"
+#include "iostream"
 #include "queue"
 #include "thread"
-#include "condition_variable"
+
 
 using namespace std;
 
-std::mutex m;
-std::condition_variable adminCV, adderCV;
-std::queue<thread::id> parkQueue;
-int size = 2;
-int tot_simulation = 15;
-int time_simulation = 0;
+mutex m;
+condition_variable cv;
+queue<thread::id> parkQueue;
+bool lastreader;
+int const NREADERS = 400;
 int readers_count;
 
-void admin_f(){
+void pushWork() {
+    int time = (rand() % 200) + 100;
+    this_thread::sleep_for(chrono::milliseconds(time));
+
     std::unique_lock<std::mutex> l{m};
+    thread::id task = this_thread::get_id();
+    parkQueue.push(task);
+    cout << "Pushed " << task << endl;
 
+    readers_count += time;
+    if(readers_count > NREADERS)
+        lastreader=true;
+
+    l.unlock();
+    cv.notify_one();
+}
+
+void workerThread() {
+
+
+    std::unique_lock<std::mutex> l{m};
     while(true) {
-        if (parkQueue.size() >= size) {
-            int time = (rand() % 4) + 4;
-            std::this_thread::sleep_for(std::chrono::seconds(time));
-            time_simulation += time;
 
+        if (!parkQueue.empty())
+        {
+            int time = (rand() % 200) + 100;
+            this_thread::sleep_for(chrono::milliseconds(time));
+            readers_count += time;
+            if(readers_count > NREADERS)
+                lastreader=true;
             thread::id task = parkQueue.front();
             parkQueue.pop();
-            cout << "==== Car out the parking " << task << "====" <<  endl;
+            cout << "==== Worked " << task << "====" <<  endl;
             l.unlock();
 
             l.lock();
-            if(parkQueue.empty()) {
+            if(parkQueue.empty() && lastreader) {
                 cout << endl <<  "**** No elements in the queue, no further readers, leaving program ****" << endl;
                 return;
             }
         }
-        if(time_simulation == tot_simulation || parkQueue.size() == size)
-            adminCV.wait(l);
+        if(!lastreader)
+            cv.wait(l);
     }
 }
-void parking_f() {
-    std::unique_lock<std::mutex> l{m};
 
-    int time = (rand() % 2) + 1;
-    std::this_thread::sleep_for(std::chrono::seconds(time));
+int main(){
 
-    parkQueue.push(this_thread::get_id());
+    thread worker(workerThread);
 
-    time_simulation += time;
-    cout << "==== Car inside the parking " << this_thread::get_id() << "====" <<  endl;
-    l.unlock();
-    adderCV.notify_one();
-}
+    vector<thread> threadPool;
+    lastreader=false;
+    readers_count=0;
 
-int main() {
+    for (int i = 1; i <= 9; ++i) {
+        if(readers_count < NREADERS)
+            threadPool.emplace_back(std::thread(pushWork));
 
-    std::vector<std::thread> cars;
-    std::thread admin(admin_f);
-
-    for(int i = 0; i < 6; i++){
-        srand((unsigned)time(NULL)); //makes seed different for calling rand()
-        cars.emplace_back(std::thread( parking_f));
     }
-
-    for(auto& i : cars) {
-        i.join();
+    for (auto& t : threadPool) {
+        t.join();
     }
+    worker.join();
 
-    adminCV.notify_one();
-    admin.join();
-    return 0;
 }
