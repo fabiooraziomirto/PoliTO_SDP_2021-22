0148a8392d5739d9aca491394375ab1ac275e54d
Fabio Orazio Mirto <fabmi1234@gmail.com>
2022-05-28 12:23:55 +0200

    add exercise 3 of lab7. TODO: fix return state after end of time_simulation

--- C++/es02lab07/cmake-build-debug/cmake_install.cmake
+++ C++/es02lab07/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/fab/CLionProjects/untitled/C++/es02lab07
+# Install script for directory: /home/fab/CLionProjects/SDP/C++/es02lab07
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/fab/CLionProjects/untitled/C++/es02lab07/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/fab/CLionProjects/SDP/C++/es02lab07/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
