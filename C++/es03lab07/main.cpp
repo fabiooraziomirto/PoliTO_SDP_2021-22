f90d162137dc88b4f86c1bd1acadd5fa3a8b1041
Fabio Orazio Mirto <fabmi1234@gmail.com>
2022-05-29 19:56:34 +0200

    add minor fix on exercise 3 of lab7

--- C++/es03lab07/main.cpp
+++ C++/es03lab07/main.cpp
@@ -10,71 +10,75 @@ mutex m;
 condition_variable cv;
 queue<thread::id> parkQueue;
 bool lastreader;
-int const NREADERS = 400;
+int size = 4;
+int const NREADERS = 5000;
 int readers_count;
 
 void pushWork() {
+    std::unique_lock<std::mutex> l{m};
     int time = (rand() % 200) + 100;
     this_thread::sleep_for(chrono::milliseconds(time));
-
-    std::unique_lock<std::mutex> l{m};
     thread::id task = this_thread::get_id();
-    parkQueue.push(task);
-    cout << "Pushed " << task << endl;
 
-    readers_count += time;
-    if(readers_count > NREADERS)
+    if((readers_count + time) > NREADERS)
         lastreader=true;
+    else if(parkQueue.size() < size){
+        readers_count += time;
+        parkQueue.push(task);
+        cout << "Pushed " << task << " size " << parkQueue.size() << endl;
+        cout << "During simulation on the push thread  " << readers_count << endl;
+        cv.notify_one();
+    } else {
+        l.unlock();
+        cv.notify_one();
+    }
 
-    l.unlock();
-    cv.notify_one();
 }
 
 void workerThread() {
-
-
     std::unique_lock<std::mutex> l{m};
     while(true) {
-
-        if (!parkQueue.empty())
-        {
-            int time = (rand() % 200) + 100;
+        if (!parkQueue.empty()) {
+            int time = (rand() % 400) + 400;
             this_thread::sleep_for(chrono::milliseconds(time));
-            readers_count += time;
-            if(readers_count > NREADERS)
+            if((readers_count + time) > NREADERS)
                 lastreader=true;
-            thread::id task = parkQueue.front();
-            parkQueue.pop();
-            cout << "==== Worked " << task << "====" <<  endl;
+            else {
+                readers_count += time;
+                thread::id task = parkQueue.front();
+                parkQueue.pop();
+                cout << "==== Pop out " << task << " size " << parkQueue.size() << endl;
+                cout << "During simulation on the working thread " << readers_count << endl;
+
+            }
             l.unlock();
 
             l.lock();
-            if(parkQueue.empty() && lastreader) {
+            if(lastreader) {
                 cout << endl <<  "**** No elements in the queue, no further readers, leaving program ****" << endl;
                 return;
             }
         }
-        if(!lastreader)
+        if(!lastreader || parkQueue.size() == size)
             cv.wait(l);
     }
+
 }
 
 int main(){
-
-    thread worker(workerThread);
-
-    vector<thread> threadPool;
     lastreader=false;
     readers_count=0;
+    thread worker(workerThread);
+    vector<thread> threadPool;
 
-    for (int i = 1; i <= 9; ++i) {
+    for (int i = 1; i <= 15; ++i) {
         if(readers_count < NREADERS)
             threadPool.emplace_back(std::thread(pushWork));
-
     }
     for (auto& t : threadPool) {
         t.join();
     }
+
     worker.join();
 
 }
